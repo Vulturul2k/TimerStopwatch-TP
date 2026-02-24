aaf729e525fa2500740811e2abb200c825fc06bc
Vulturul2k <94383678+Vulturul2k@users.noreply.github.com>
2026-02-24 15:04:23 +0100

    State-based variable implementation:

--- src/main/java/states/timer/RingingTimer.java
+++ src/main/java/states/timer/RingingTimer.java
@@ -3,24 +3,40 @@ package states.timer;
 import states.ClockState;
 
 public class RingingTimer extends ActiveTimer {
- 	
-	// use Singleton design pattern
-	private RingingTimer() {}; // make constructor invisible to clients
+
+    // use Singleton design pattern
+    private RingingTimer() {
+    }; // make constructor invisible to clients
+
     private static RingingTimer instance = null;
+
     public static RingingTimer Instance() {
-        if(instance == null) instance = new RingingTimer();        
+        if (instance == null)
+            instance = new RingingTimer();
         return instance;
     }
-    
+
+    @Override
+    protected void entry() {
+        super.entry();
+        ringing = true;
+    }
+
+    @Override
+    protected void exit() {
+        super.exit();
+        ringing = false;
+    }
+
     @Override
     public ClockState doIt() {
-    	java.awt.Toolkit.getDefaultToolkit().beep();
-    	return this;
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        return this;
     }
-    
+
     public String getDisplayString() {
-    	// display decreasing values starting from memTimer counting down to 0
+        // display decreasing values starting from memTimer counting down to 0
         return "Time's up !";
     }
-    
+
 }
