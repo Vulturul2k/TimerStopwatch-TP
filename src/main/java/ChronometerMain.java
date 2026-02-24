7a6bc5c4260c84c30080cb997e97e324ba17d870
Vulturul2k <94383678+Vulturul2k@users.noreply.github.com>
2026-02-24 14:55:00 +0100

    adding logging

--- src/main/java/ChronometerMain.java
+++ src/main/java/ChronometerMain.java
@@ -1,33 +1,41 @@
 import gui.SwingGUI;
 import states.Context;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ChronometerMain {
-    
-	private SwingGUI g;
-	private Context c;
-   	
+
+    private static final Logger LOGGER = Logger.getLogger(ChronometerMain.class.getName());
+
+    private SwingGUI g;
+    private Context c;
+
     // The method run() ensures that with a given frequency
     // the state machine's actions are executed with tick() and
-    // the ui is updated accordingly with updateUIText().    
-	private void run(int frequency) {
+    // the ui is updated accordingly with updateUIText().
+    private void run(int frequency) {
         // infinite loop that asks the current state to do whatever it needs to do
         // and that updates the graphical user interface accordingly
- 		  g.updateUI(c);
-    	  while (true) {
-    		try { Thread.sleep(frequency); }
-    		catch (InterruptedException e) { e.printStackTrace(); }
- 	        g.updateUI(c);
- 	        c.tick();
-  	      }
-      }
-    
+        g.updateUI(c);
+        while (true) {
+            try {
+                Thread.sleep(frequency);
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.WARNING, "Thread interrupted", e);
+            }
+            g.updateUI(c);
+            c.tick();
+        }
+    }
+
     public static void main(String[] args) {
         ChronometerMain myChrono = new ChronometerMain(); // create an instance of Chronometer;
         myChrono.c = new Context(); // create the state machine context
         myChrono.g = new SwingGUI(myChrono.c); // create the GUI and pass it the context
-        
+
         myChrono.run(100); // and start running with frequency of 100 millisecs
-        
+
     }
-   
+
 }
